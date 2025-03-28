f01911906e790bdd5770b04af0c68661deb84f5f
André Corrêa <andrencs93@gmail.com>
2025-03-28 22:10:04 +0100

    added BreakingBestAndWorstRecords.ts

---
+++ Prepare/Algorithms/Implementation/BreakingBestAndWorstRecords.ts
@@ -0,0 +1,19 @@
+function breakingRecords(scores: number[]): number[] {
+  // Write your code here
+
+  const records = [0, 0];
+  let min = scores[0];
+  let max = scores[0];
+
+  for (let i = 1; i < scores.length; i++) {
+    if (scores[i] < min) {
+      min = scores[i];
+      records[1]++;
+    } else if (scores[i] > max) {
+      max = scores[i];
+      records[0]++;
+    }
+  }
+
+  return records;
+}
